6414649efb4d567510c7ab5bcadcc1a38b8fdb6c
subrota-metaforktech <subrota.metaforktech@gmail.com>
2025-11-26 21:24:14 +0600

    server update

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -9,8 +11,11 @@ const port = process.env.PORT || 3000;
 
 
 // middleware
-app.use(cors());
+app.use(cors({ origin: "https://admin-subrota.vercel.app", credentials: true, allowedHeaders: ["Content-Type", "Authorization"], }));
 app.use(express.json());
+const JWT_SECRET = process.env.JWT_SECRET;
+const ADMIN_EMAIL = process.env.ADMIN_EMAIL;
+const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wybojxh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -21,6 +26,40 @@ const client = new MongoClient(uri, {
         strict: true,
         deprecationErrors: true,
     }
+});
+
+function auth(req, res, next) {
+    const header = req.headers.authorization;
+    if (!header) return res.status(403).json({ message: "No token provided" });
+
+    const token = header.split(" ")[1];
+
+    jwt.verify(token, JWT_SECRET, (err, decoded) => {
+        if (err) return res.status(403).json({ message: "Invalid token" });
+        req.user = decoded;
+        next();
+    });
+}
+
+app.use((req, res, next) => {
+    console.log("AUTH HEADER =>", req.headers.authorization);
+    next();
+});
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    if (email !== ADMIN_EMAIL) {
+        return res.status(401).json({ message: "Invalid email" });
+    }
+
+    const isMatch = await bcrypt.compare(password, ADMIN_PASSWORD);
+    if (!isMatch) {
+        return res.status(401).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign({ email }, JWT_SECRET, { expiresIn: "2h" });
+    res.json({ token });
 });
 async function run() {
     try {
@@ -54,6 +93,45 @@ async function run() {
                 res.send(project);
             } catch (error) {
                 res.status(500).send({ message: "Error fetching project", error: error.message });
+            }
+        });
+        app.post("/projects", auth, async (req, res) => {
+            const newProject = req.body;
+            const result = await projectCollection.insertOne(newProject);
+            res.send(result);
+        });
+        app.delete("/projects/:id", auth, async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await projectCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        app.put("/projects/:id", auth, async (req, res) => {
+            try {
+                const id = req.params.id;
+                const updatedProject = req.body;
+                const filter = { _id: new ObjectId(id) };
+
+                const updateDoc = {
+                    $set: {
+                        name: updatedProject.name,
+                        image: updatedProject.image,
+                        description: updatedProject.description,
+                        stack: updatedProject.stack,
+                        liveLink: updatedProject.liveLink,
+                        githubLink: updatedProject.githubLink,
+                        challenges: updatedProject.challenges,
+                        futurePlans: updatedProject.futurePlans,
+                        status: updatedProject.status,
+                    },
+                };
+
+                const result = await projectCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            } catch (error) {
+                console.error("Error updating project:", error);
+                res.status(500).send({ error: "Failed to update project" });
             }
         });
 
